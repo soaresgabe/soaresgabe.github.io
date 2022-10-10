603cba1f4d01e607acfe4de0c4f6aa35497e6e20
soaresgabe <gabesoares@proton.me>
2022-10-10 18:14:08 -0300

    v1.6

---
+++ src/components/bubbles/Bubbles.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Bubbles.css'
+
+function Bubbles() {
+  return (
+<ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+</ul>
+)}
+
+export default Bubbles
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,10 +3,12 @@ import Avatar from "./Avatar";
 import NavBar from "../nav/NavBar";
 import HeaderSocials from "../socials/HeaderSocials";
 import './header.css'
+import Bubbles from "../bubbles/Bubbles";
 
 function Header() {
   return <header className="top-container">
 
+    <Bubbles/>
     <div className="head" id="home">
       <h1 className="msg">Hello, I'm</h1>
       <h1 className="name">Gabe Soares</h1>
